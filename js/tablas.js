9853559818bc921e09d7421261b8873e3f4c31cc
TVM23 <99370198+TVM23@users.noreply.github.com>
2023-04-20 18:54:42 -0600

    frontend

---
+++ js/tablas.js
@@ -0,0 +1,19 @@
+$(function() {
+    $(document).ready(function() {
+      $('#example').DataTable({
+        "language": {
+          "lengthMenu": "Mostrando _MENU_ registros por página",
+          "zeroRecords": "No se encontro ningun registro",
+          "info": "Mostrando la pagina _PAGE_ de un total de _PAGES_ paginas",
+          "infoEmpty": "No hay registros disponibles",
+          "infoFiltered": "(Filtrado de _MAX_ registros totales)",
+          'search': "Buscar: ",
+          'paginate': {
+            'next': "Siguiente",
+            'previous': 'Anterior'
+          }
+      }
+      });
+      
+    });
+  });
